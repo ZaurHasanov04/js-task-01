e73f22b701d1ae5ec8597bd2f04ba967119d3720
ZaurHasanov04 <41968288+ZaurHasanov04@users.noreply.github.com>
2020-11-06 17:05:28 +0400

    updated version form validation v1.1

--- js/app.js
+++ js/app.js
@@ -1,120 +1,112 @@
-/*
-submitBtn=document.querySelector("#submitbtn")
-submitBtn.addEventListener("click",function(){
-   userName=document.querySelector('.name')
-   userSurname=document.querySelector('.surname')
-   userEmail=document.querySelector('.mail')
-   userBirthDate=document.querySelector('.birthDate')
-   userSurname=document.querySelector('.userSurname')
-   userEmail=document.querySelector('.userEmail')
-   userBirthDate=document.querySelector('.userBirthDate')
-   user=new UserData(userName.value,userSurname.value,userEmail.value,userBirthDate.value
-*/
 
-data=[]
+// function validation(){
+  
+//    userName=document.querySelector('.name');
+//    userSurname=document.querySelector('.surname');
+//    userBirthDate=document.getElementById("birthday");
+//    userGender=document.querySelector('.gender');
+//    userEmail=document.querySelector('.mail');
+//    userNumber=document.querySelector('.number');
+//    userSelect=document.querySelector('.numbers');
 
-class UserData{
-   constructor(_name,_surname,_date,_gender,_email,_phone,_select){
-       this.Name=_name;
-       this.Surname=_surname;
-       this.Date=_date;
-       this.Gender=_gender;
-       this.Email=_email;
-       this.Phone=_phone;
-       this.Select=_select;
-       
-   }
+//    console.log(userName.value + " / " + userSurname.value + " / " + userBirthDate.value  + "-"+ userGender.value + "-" + userEmail.value + "-" + userNumber.value + "-" + userSelect.value)
 
-   showData(){
-       console.log(this.Name + " / " +this.Surname + "/"+ this.Date + " / " +this.Gender + "/" + this.Email + " / " +this.Phone + "/" + this.Select) 
-   }
-}
+
+// }
 
 
 
-function validate() {
-      
-   if( document.myForm.name.value == "" ) {
-      alert( "Adinizi yazin!" );
-      document.myForm.name.focus() ;
-      return false;
-   }
-   if( document.myForm.surname.value == "" ) {
-      alert( "Soyadinizi yazin" );
-      document.myForm.surname.focus() ;
-      return false;
-   }
-
-   if(document.myForm.birthdate.value==""){
-      alert("Tevelludu yazin");
-      document.myForm.birthdate.focus();
-      return false
-   }
-
-
-   if( document.myForm.mail.value ==""){
-      alert("Emailinizi yazin");
-      document.myForm.mail.focus();
-      return false;
-   }
-
-
-   
-
-
-   var num=document.myForm.number.value
-
-
-   if(num.length<=6){
-      alert("Telefon nomreniz yazin ve ya nomre duzgun deyil");
-      document.myForm.number.focus();
-      return false;
-   }
-
-   if( document.myForm.numbers.value === "-1" ) {
-      alert( "Secim xanasini tenzimleyin" );
-      
-      return false;
-   }
-   return( true );
-
-
-   
-}
-
-
-
-
-function emailvalidation(){
-   var emailID = document.myForm.EMail.value;
-   post1 = emailID.indexOf("@");
-    post2= emailID.lastIndexOf(".");
-   
-   if (post1 < 1 || ( post2 - post1 < 2 )) {
-      alert("Mail dogru deyi")
-      document.myForm.mail.focus() ;
-      return false;
-   }
-   return true;
-}
-
-
-submitBtn=document.querySelector("#submitbtn");
-submitBtn.addEventListener("click", function()
-{
+function validation(){
+  
    userName=document.querySelector('.name');
    userSurname=document.querySelector('.surname');
-   userBirthDate=document.querySelector('.birthdate');
+   userBirthDate=document.getElementById("birthday");
    userGender=document.querySelector('.gender');
    userEmail=document.querySelector('.mail');
    userNumber=document.querySelector('.number');
    userSelect=document.querySelector('.numbers');
+
+}
+
+function phone(){
+   if(document.myForm.number.value==""){
+      alert("Nomrenizi yazin")
+      document.myForm.number.focus();
+   }
+
+   if(document.myForm.number.value.length<6){
+      alert("Nomre duzgun deyil")
+   }
+
+   if(document.myForm.numbers.value=="txt"){
+      alert("Sec")
+      return false;
+   }
+   return(true)
+}
+
+
+function myform(){
+   if(document.myForm.name.value==""){
+      alert("Adinizi yazmamisiz !")
+      document.myForm.name.focus();
+      return false;
+   }
+
+   if(document.myForm.surname.value==""){
+      alert("Soyadinizi yazin")
+      document.myForm.surname.focus();
+      return false;
+   }
+
+   var a = document.getElementById("birthday");
+   var defaultval=a.defaultValue;
+   var realval=a.value
+
+   if(defaultval == realval){
+      alert("Birthday yaz")
+      return false;
+   }
+
+   if(document.getElementsByName("gender")[0].checked==false && document.getElementsByName("gender")[1].checked==false){
+      alert("Cins bolmesin tamamlayin")
+      return ;
+   }
+
+   if(document.myForm.mail.value==""){
+      alert("Mailinizi yazin")
+      document.myForm.mail.focus();
+      return false;
+   }
+
+   if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.mail.value)){
+      return true;
+   }
+
+   else{ 
+      alert("mailinizi duz yazin @ ve noqte olmalidi");
+      document.myForm.mail.focus();
+      return false;
+   } 
+
   
-   user= new UserData(userName.value, userSurname.value, userBirthDate.value, userGender.value, userEmail.value, userNumber.value, userSelect.value);
 
-   data.push(user);
-   user.showData();
+}
+
+function check(){
+   if(myform()==true && phone()==true){
+      validation();
+      console.log(userName.value + " / " + userSurname.value + " / " + userBirthDate.value  + "-"+ userGender.value + "-" + userEmail.value + "-" + userNumber.value + "-" + userSelect.value)
+ 
+   }
+}
+
+
+      // console.log(userName.value + " / " + userSurname.value + " / " + userBirthDate.value  + "-"+ userGender.value + "-" + userEmail.value + "-" + userNumber.value + "-" + userSelect.value)
    
-   console.log(userName.value + " / " +userSurname.value + "/"+ userBirthDate.value + " / " +userGender.value + "/" + userEmail.value + " / " + userNumber.value+ "/" + userSelect.value) 
 
-})
+
+
+
+
+
